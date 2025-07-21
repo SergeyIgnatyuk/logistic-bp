15bf42f06a7e8ec171cb2b5eca541cd5b80d7733
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2025-07-21 11:34:59 +0300

    добавил авторизацию между сервисами

--- src/main/java/by/logisticbp/interceptor/UserContextRequestInterceptor.java
+++ src/main/java/by/logisticbp/interceptor/UserContextRequestInterceptor.java
@@ -1,15 +1,24 @@
 package by.logisticbp.interceptor;
 
+import by.logisticbp.client.KeycloakTokenClient;
 import by.logisticbp.utils.UserContext;
 import by.logisticbp.utils.UserContextHolder;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserContextRequestInterceptor implements RequestInterceptor {
+
+    private final KeycloakTokenClient tokenClient;
+
     @Override
     public void apply(RequestTemplate requestTemplate) {
+        String token = tokenClient.getAccessToken();
+        requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + token);
         requestTemplate.header(UserContext.CORRELATION_ID, UserContextHolder.getContext().getCorrelationId());
     }
 }
